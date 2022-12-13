c35f6c0cf68c2a362934ba3e288410c9fa611ab6
Octavio Lara <octavioasapchi@gmail.com>
2022-12-13 01:56:49 -0400

    [:sparkles: ADD] Authentication with jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ClubModule } from './club/club.module';
 import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [AuthModule, UserModule, ClubModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    ClubModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,11 +1,17 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { hash, verify } from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import { argon2d, hash, verify } from 'argon2';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ConfigService } from '@nestjs/config';
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     // Generate the password hash
     const passHash = await hash(dto.password);
@@ -18,8 +24,7 @@ export class AuthService {
           clubId: 1,
         },
       });
-      delete user.hash;
-      return user;
+      return this.signToken(user.id, user.email, user.role);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -42,7 +47,18 @@ export class AuthService {
     if (!pwMatches) {
       throw new ForbiddenException('No match');
     }
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email, user.role);
+  }
+  signToken(userId: number, email: string, role: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
+      role,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    return this.jwt.signAsync(payload, {
+      expiresIn: '12h',
+      secret,
+    });
   }
 }
