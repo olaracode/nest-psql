12576ebbd6271b37bb68f2e8b40e395a5ce56e0c
Octavio Lara <octavioasapchi@gmail.com>
2022-12-13 01:16:56 -0400

    [:sparkles: ADD] create user and login user
    [:construction: ADD] db relationships
    
    [:construction: ADD] db migration scripts to package.json

---
+++ prisma/migrations/20221213045648_migrate_models/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Club` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Club";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "membership" TEXT,
+    "role" TEXT NOT NULL,
+    "clubId" INTEGER NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "clubs" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+
+    CONSTRAINT "clubs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_clubId_fkey" FOREIGN KEY ("clubId") REFERENCES "clubs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,12 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { argon2d, hash, verify } from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return 'Sign up';
+  async signup(dto: AuthDto) {
+    // Generate the password hash
+    const passHash = await hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: passHash,
+          role: 'admin',
+          clubId: 1,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already taken');
+        }
+      }
+      throw error;
+    }
   }
-  signin() {
-    return 'Sign in';
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('No match');
+    }
+    const pwMatches = await verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('No match');
+    }
+    delete user.hash;
+    return user;
   }
 }
